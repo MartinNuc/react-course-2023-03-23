b653d1b412d0a07b97f1e4c4e9efd4c030e4067a
MartinNuc <martin.nuc@productboard.com>
2023-03-23 15:14:15 +0100

    fix format in counter button

--- src/counter-button/CounterButton.js
+++ src/counter-button/CounterButton.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 export const CounterButton = () => {
   const [counter, setCounter] = useState(0);
 
-return <button onClick={() => setCounter(counter + 1)}>
+  return <button onClick={() => setCounter(counter + 1)}>
     {counter}x clicked
   </button>
 }
